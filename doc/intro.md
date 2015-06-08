6af54e26f0b5017413959ed4709da58c3f55cfb9
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-06-08 19:49:53 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to social_kata
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/social_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns social-kata.core-test
+  (:require [clojure.test :refer :all]
+            [social-kata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
