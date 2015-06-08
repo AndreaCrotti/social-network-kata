974b841ee5db70cb21e8c7004d08b9875e17061b
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-06-08 20:19:51 +0100

    handler

--- src/social_kata/handler.clj
+++ src/social_kata/handler.clj
@@ -1,10 +1,13 @@
 (ns social-kata.handler
   (:require [compojure.core :refer :all]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults site-defaults]]
+            [social-kata.core :refer :all]
             [compojure.route :as route]))
 
 (defroutes app-routes
   (GET "/" [] "Welcome to social the network")
+  (GET "/tweets" [username] (get-tweets username))
+  (POST "/tweet" [username message] (post-tweet username message))
   (route/not-found "Not found"))
 
 (def app
