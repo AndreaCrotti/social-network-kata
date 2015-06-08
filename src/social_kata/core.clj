ab5a7b9d5bdf3e6adb5da90741835547102084bf
Alex Curtis <alex@alexandercurtis.net>
2015-06-08 20:07:36 +0100

    Add post function

--- src/social_kata/core.clj
+++ src/social_kata/core.clj
@@ -1,5 +1,17 @@
 (ns social-kata.core)
 
+(def timelines (atom {}))
+
+(defn add-tweet
+  [a-timeline username tweet]
+  (merge-with concat a-timeline {username [tweet]}))
+
+
+(defn post
+  [username tweet]
+  (swap! timelines add-tweet username tweet))
+
+
 (defn foo
   "I don't do a whole lot."
   [x]
